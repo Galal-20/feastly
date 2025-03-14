c7a324456c1ed42eb1deb2eae665edc045306178
En.Galal Ahmed <ga71387@gmail.com>
2025-03-15 01:07:44 +0200

    Fixed bugs

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,5 +1,13 @@
 class Assets {
 
+  static const AssetsSvgHome = 'assets/svg/home.svg';
+
+  static const AssetsSvgUser = 'assets/svg/user.svg';
+
+  static const AssetsSvgHeart = 'assets/svg/heart.svg';
+
+  static const AssetsSvgSearch = 'assets/svg/search.svg';
+
   static const AssetsFontsInter = 'assets/fonts/Inter.zip';
 
   static const AssetsFontsInter24ptBold = 'assets/fonts/Inter_24pt-Bold.ttf';
@@ -10,7 +18,25 @@ class Assets {
 
   static const AssetsFontsInter24ptSemibold = 'assets/fonts/Inter_24pt-SemiBold.ttf';
 
+  static const AssetsImages2xStar = 'assets/images/@2x/Star.png';
+
+  static const AssetsImages2xNotification = 'assets/images/@2x/Notification.png';
+
+  static const AssetsImages3xStar = 'assets/images/@3x/Star.png';
+
+  static const AssetsImages3xNotification = 'assets/images/@3x/Notification.png';
+
   static const AssetsImagesBg = 'assets/images/bg.png';
+
+  static const AssetsImagesLogo = 'assets/images/logo.png';
+
+  static const AssetsImagesStar = 'assets/images/Star.png';
+
+  static const AssetsImagesDish1 = 'assets/images/dish1.png';
+
+  static const AssetsImagesDish2 = 'assets/images/dish2.png';
+
+  static const AssetsImagesLogo2 = 'assets/images/logo2.png';
 
   static const AssetsImagesAppLogo = 'assets/images/app_logo.png';
 
@@ -24,23 +50,17 @@ class Assets {
 
   static const AssetsImagesOnboarding3 = 'assets/images/onboarding3.png';
 
+  static const AssetsImagesNotification = 'assets/images/Notification.png';
+
   static const AssetsAnimationsAnimation = 'assets/animations/animation.json';
 
   static const AssetsLocalizationAr = 'assets/localization/ar.json';
 
   static const AssetsLocalizationEn = 'assets/localization/en.json';
-
   static const app_logo = 'assets/images/app_logo.png';
   static const onBoarding1 = 'assets/images/onboarding1.png';
   static const onBoarding2 = 'assets/images/onboarding2.png';
   static const onBoarding3 = 'assets/images/onboarding3.png';
 }
 
-/*
-* class Assets {
-  static const AssetsAnimationsAnimation = 'assets/animations/animation.json';
-  static const app_logo = 'assets/images/app_logo.png';
-  static const onBoarding1 = 'assets/images/onboarding1.png';
-  static const onBoarding2 = 'assets/images/onboarding2.png';
-  static const onBoarding3 = 'assets/images/onboarding3.png';
-}*/
+
