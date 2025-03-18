4893ce787d59ceafd94e07fc992bfa9ab010f1cd
Ahmed Gamal <ahmedgamal5179@gmail.com>
2025-03-18 12:50:40 +0200

    provided bloc to the body

--- lib/src/features/ai_chat/presentation/views/ai_chat_screen.dart
+++ lib/src/features/ai_chat/presentation/views/ai_chat_screen.dart
@@ -1,6 +1,8 @@
+import 'package:feastly/src/features/ai_chat/presentation/view_model/ai_chat_bloc/ai_chat_bloc.dart';
 import 'package:feastly/src/features/ai_chat/presentation/widgets/ai_chat_screen_body.dart';
 import 'package:feastly/src/features/ai_chat/presentation/widgets/custom_ai_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AiChatScreen extends StatelessWidget {
   const AiChatScreen({super.key});
@@ -10,7 +12,10 @@ class AiChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildCustomAiAppBar(context),
       backgroundColor: Colors.white,
-      body: AiChatScreenBody(),
+      body: BlocProvider(
+        create: (context) => AiChatBloc(),
+        child: AiChatScreenBody(),
+      ),
     );
   }
 }
