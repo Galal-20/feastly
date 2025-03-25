d580934ad9f84c687ed1b633fd2adc4582468144
Ahmed_Tanany <ahmedtanany25@gmail.com>
2025-03-26 00:35:51 +0200

    search update.

--- lib/src/core/notification/WorkManagerService.dart
+++ lib/src/core/notification/WorkManagerService.dart
@@ -1,32 +1,32 @@
-import 'package:workmanager/workmanager.dart';
-
-import 'notification_service.dart';
-
-class WorkManagerService {
-  void registerMyTask() async {
-    //register my task
-    await Workmanager().registerPeriodicTask(
-      'id1',
-      'show simple notification',
-      frequency: const Duration(minutes: 24), // Run every 24 hours
-    );
-  }
-
-  //init work manager service
-  Future<void> init() async {
-    await Workmanager().initialize(actionTask, isInDebugMode: true);
-    registerMyTask();
-  }
-
-  void cancelTask(String id) {
-    Workmanager().cancelAll();
-  }
-}
-
-@pragma('vm-entry-point')
-void actionTask() {
-  Workmanager().executeTask((taskName, inputData) {
-    LocalNotificationService.scheduleDailyNotifications();
-    return Future.value(true);
-  });
-}
+// import 'package:workmanager/workmanager.dart';
+//
+// import 'notification_service.dart';
+//
+// class WorkManagerService {
+//   void registerMyTask() async {
+//     //register my task
+//     await Workmanager().registerPeriodicTask(
+//       'id1',
+//       'show simple notification',
+//       frequency: const Duration(minutes: 24), // Run every 24 hours
+//     );
+//   }
+//
+//   //init work manager service
+//   Future<void> init() async {
+//     await Workmanager().initialize(actionTask, isInDebugMode: true);
+//     registerMyTask();
+//   }
+//
+//   void cancelTask(String id) {
+//     Workmanager().cancelAll();
+//   }
+// }
+//
+// @pragma('vm-entry-point')
+// void actionTask() {
+//   Workmanager().executeTask((taskName, inputData) {
+//     LocalNotificationService.scheduleDailyNotifications();
+//     return Future.value(true);
+//   });
+// }
